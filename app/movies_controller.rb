7833839db3d5b028547bd545744def8b28cf4034
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-11 19:09:50 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args =
+def can_be_created_in_a_block(args = {
     title: "The Sting",
     release_date: 1973,
     director: "George Roy Hill",
