f473b110ebbf1f01e0c6e8468c871466d4892b20
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-11 19:21:50 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ args = {
     in_theaters: false
 }
 
-def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: '1990'})
+def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
