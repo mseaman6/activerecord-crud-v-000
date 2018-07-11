c27310d2fe9f6f1779c364bd173e8a43420e9bc1
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-11 18:41:40 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = nil)
-  movie = Movie.create do |movie|
+  Movie.create do |movie|
     movie.title =
     movie.release_date = 
   end
