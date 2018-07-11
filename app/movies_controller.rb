fc2736d0ba764726614cefaf8a192243bf6e753b
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-11 19:01:35 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,11 @@ end
 
 def can_be_created_in_a_block(args = nil)
   Movie.create do |m|
-    
+    m.title = 
+    m.release_date = 
+    m.director = 
+    m.lead = 
+    m.in_theaters = 
   end
 end
 
