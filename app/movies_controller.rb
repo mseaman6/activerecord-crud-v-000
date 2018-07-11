1e16d209dc17f5f68ca3b28b172f4e9644dbcad6
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-11 17:53:45 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,13 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {
-    title: "The Sting",
-    release_date: 1973,
-    director: "George Roy Hill",
-    lead: "Paul Newman",
-    in_theaters: false
-})
+def can_be_created_in_a_block(args = {})
   Movie.create(args)
 end
 
