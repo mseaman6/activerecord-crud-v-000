6d5034bafa5f76cb06d3e3c585726616c189c11b
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-11 19:12:51 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,13 +22,15 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {
+args = {
     title: "The Sting",
     release_date: 1973,
     director: "George Roy Hill",
     lead: "Paul Newman",
     in_theaters: false
-})
+}
+
+def can_be_created_in_a_block(args = nil)
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
